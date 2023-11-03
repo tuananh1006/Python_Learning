adb6235a61b89bec43c007f2e67acc81da2cdbde
Tuan Anh <eszss56@gmail.com>
2023-11-04 00:12:57 +0700

    version1

---
+++ greeting.py
@@ -0,0 +1,23 @@
+##--Python Getting Started
+#ctrl shift p -> to adjust default compiler and see version
+#-------------------------
+#press py -3 --version to view version
+#print py in cmd is REPL , which can help to run code at terminal
+#-------------------------
+
+#chapter 1-Getting Started
+greeting='Hello World'
+print(greeting)
+
+##--python basic
+line01="************"
+line02="*          *"
+line03="* WELCOME !*"
+
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+#shift alt down to duplicate row
