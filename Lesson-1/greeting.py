0e87aae2bdc8275a63024df174da34a4d04d7377
Tuan Anh <eszss56@gmail.com>
2023-11-04 10:43:58 +0700

    day2-operator

--- Lesson-1/greeting.py
+++ Lesson-1/greeting.py
@@ -1,18 +1,22 @@
-##--Python Getting Started
-#ctrl shift p -> to adjust default compiler and see version
-#-------------------------
-#press py -3 --version to view version
-#print py in cmd is REPL , which can help to run code at terminal
-#-------------------------
+# --Python Getting Started
+# ctrl shift p -> to adjust default compiler and see version
+# -------------------------
+# press py -3 --version to view version
+# print py in cmd is REPL , which can help to run code at terminal
+# -------------------------
 
-#chapter 1-Getting Started
-greeting='Hello World'
+# chapter 1-Getting Started
+greeting = 'Hello World'
 print(greeting)
 
-##--python basic
-line01="************"
-line02="*          *"
-line03="* WELCOME !*"
+# --python basic
+line01 = "************"
+line02 = "*          *"
+line03 = "* WELCOME !*"
+line04 = "***"
+
+
+#
 
 print('')
 print(line01)
@@ -20,4 +24,4 @@ print(line02)
 print(line03)
 print(line02)
 print(line01)
-#shift alt down to duplicate row
+# shift alt down to duplicate row
