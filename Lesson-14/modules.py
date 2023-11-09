634c02e208d32ffddd70a1af5c40e51cbc96ee53
Tuan Anh <eszss56@gmail.com>
2023-11-09 17:38:43 +0700

    Add modules.py and rps7.py

---
+++ Lesson-14/modules.py
@@ -0,0 +1,24 @@
+from rps7 import rock_paper_scissors
+import sys
+import math
+import random as rdm
+from enum import Enum
+import kansas
+print(math.pi)
+
+# sys.exit()
+# random.choice("123")
+# print(dir(rdm))
+
+# for item in dir(rdm):
+#    print(item)
+
+
+print(kansas.capital)
+# kansas.randomfunfact()
+
+print(__name__)
+
+print(kansas.__name__)
+
+rock_paper_scissors()
